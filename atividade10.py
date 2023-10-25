1d7f24b411572f785eb77597d861a1919d5e7fd2
erikedu10 <148131588+erikedu10@users.noreply.github.com>
2023-10-25 10:40:23 -0300

    Add files via upload

---
+++ atividade10.py
@@ -0,0 +1,7 @@
+vel = int(input("digite a velocidade em km/h"))
+
+if vel > int("60"):
+    print("você recebeu uma multa por excesso de velocidade")
+else:
+    print("você está dentro da velocidade permitida")
+
